3842cc99642e7e2ea42976b007304dd903101494
KangHyeonmyeong <buckgusaud@gmail.com>
2019-08-08 03:40:59 +0900

    ejs설정 및 정적리소르 경로 설정

--- nodeServer/Server/main.js
+++ nodeServer/Server/main.js
@@ -3,15 +3,21 @@ var express = require("express");               //express프레임워크
     app = express();                            //설정
     path=require("path");                       //경로설정 컴포넌트?
     ejs = require("ejs");                       //뷰 엔진
-    router = require("./routes/mainController"); 
+    router = require("./routes/pageRouter"); 
 
-app.set('view engine', 'ejs');      //서버 뷰 엔진 설정
-app.set('views', 'views');          //view경로 설정 (default가 views라고는 함)
+app.set('view engine', 'ejs');                  //서버 뷰 엔진 설정
+app.set('views', './Server/views');             //view경로 설정 (default가 views라고는 함)
+
+app.use(express.json());                        //express에 내장 되어있는 parser 사용.
 app.use(router);
-var fs = require("fs");             //파일 설정
+
+//라우터 설정보다 및에 있어야 Http 요청시 위 설정의 결로를 우선 탐색하여 더 빠른 응답속도를 보여준다.
+app.use(express.static('./Server/page.js'));             
+var fs = require("fs");                         //파일 설정
 
 const httpServer = require("http").createServer(app);
 httpServer.listen( 8080, ()=>{
+    console.log('aaa    ;   ');
     console.log("Starting Server is Complete.");
     console.log("Listening... ");
 });
--- nodeServer/Server/routes/pageRouter.js
+++ nodeServer/Server/routes/pageRouter.js
@@ -5,7 +5,7 @@ var ejs = require("ejs");
 
 router.get("/dashboard", function( req, res){
     console.log("Main Page called.");
-    res.render("");
+    res.render("home");
 });
 
 module.exports = router;        //requier를 통해 호출되면 이 모듈이 리턴된다?
